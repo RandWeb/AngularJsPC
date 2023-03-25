7d9ab4a7387271f3652fc4e1c2407167b5d13f0e
مهرداد تابش <mehrdadit12@gmail.com>
2023-03-25 03:42:08 -0700

    modules

---
+++ wintellCourse/Fundamentals/lesson7Models/Controllers.js
@@ -0,0 +1,12 @@
+﻿var myControllers = angular.module('myApp.controllers', []);
+
+myControllers.controller('ModuleController', function($scope, $window, appTime) {
+    $scope.moduleName = 'Learn about Modules';
+    $scope.appRunning = appTime();
+    $scope.refresh = function() {
+        $scope.appRunning = appTime();
+    };
+    $scope.home = function() {
+        $window.location.href = '../Index.html';
+    };
+});
---
+++ wintellCourse/Fundamentals/lesson7Models/Modules.js
@@ -0,0 +1,16 @@
+﻿var myApp = angular.module('myApp', ['myApp.controllers']);
+
+myApp.value('appName', 'Fundamentals of AngularJS');
+
+myApp.run(function ($rootScope, appName) {
+    $rootScope.appName = appName;
+    $rootScope.startTime = new Date();
+});
+
+myApp.factory('appTime', function($rootScope) {
+    return function() {
+        var now = new Date();
+        return now - $rootScope.startTime;
+    };
+});
+
