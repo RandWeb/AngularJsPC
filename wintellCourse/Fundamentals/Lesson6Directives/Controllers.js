e021097e56879d940c81cd6be07a3a80831f6305
مهرداد تابش <mehrdadit12@gmail.com>
2023-03-25 02:56:03 -0700

    directives

---
+++ wintellCourse/Fundamentals/Lesson6Directives/Controllers.js
@@ -0,0 +1,22 @@
+﻿var myControllers = angular.module('myApp.controllers', []);
+
+myControllers.controller('DirectiveController', function ($scope) {
+    $scope.secured = true;
+    $scope.notSecured = true;
+    $scope.disallow = function() {
+        $scope.notSecured = false;
+    };
+    $scope.names = [{
+            firstName: 'Jeremy',
+            lastName: 'Likness'
+        }, {
+            firstName: 'Jeff',
+            lastName: 'Prosise'
+        },{
+            firstName: 'Jeffrey',
+            lastName: 'Richter'
+        }, {
+            firstName: 'John',
+            lastName: 'Robbins'
+        }];
+});
---
+++ wintellCourse/Fundamentals/Lesson6Directives/Directives.js
@@ -0,0 +1,28 @@
+﻿var myDirectives = angular.module('myApp.directives', []);
+
+myDirectives.directive('ngSecured', function($compile) {
+    var definition = {
+        restrict: 'A',
+        scope: false,
+        link: function(scope, element, attrs) {
+            scope.$watch(attrs.ngSecured, function(value) {
+                if (value) {
+                    return;
+                }
+
+                var e = $compile('<span>Sorry, this content is restricted.</span>')(scope);
+                element.replaceWith(e);
+            });
+        }
+    };
+    return definition;
+});
+
+myDirectives.directive('ngGridList', function() {
+    var definition = {
+        restrict: 'E',
+        replace: true,
+        templateUrl: 'Template.html',        
+    };
+    return definition;
+});
